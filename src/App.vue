4e5566c77d5d4b001cd7fe9a6f6df48d1526f73a
Paula Gajardo <paulagajardosch@gmail.com>
2026-02-20 23:31:37 -0300

    Ajustando Maquetación y Crando Carpeta dist

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-<PizzaGridComponent :pizzas="pizzas" />
+<NavBarComponent/>
+<HeaderComponent/>
+<PizzaGridComponent :pizzas="pizzas"
+  @ver-detalle="verDetalle" />
 
 <PizzaModalComponent v-if="mostrarModal"
   :pizza="pizzaSeleccionada"
   @cerrar="mostrarModal = false"
 />
 
-<PizzaGrid 
-  :pizzas="pizzas"
-  @ver-detalle="verDetalle"
-/>
+<FooterComponent/>
 
 </template>
 
@@ -17,15 +17,18 @@
 import PizzaGridComponent from './components/PizzaGridComponent.vue';
 import pizzaData from '../src/assets/data/pizzas.json'
 import { ref } from 'vue'
+import NavBarComponent from './components/NavBarComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
 const pizzas = ref(pizzaData)
 
 const mostrarModal = ref(false)
 const pizzaSeleccionada = ref(null)
 
-/*const verDetalle = (id)=>{
+const verDetalle = (id)=>{
   pizzaSeleccionada.value = pizzas.value.find(p => p.id === id)
   mostrarModal.value = true
-}*/
+}
 
 
 </script>
